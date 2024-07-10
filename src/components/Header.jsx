2e3328ec56322a39d17b0242ccb401cb985d1679
Dee Cee <153252185+dcho10@users.noreply.github.com>
2024-07-10 15:08:02 -0400

    Finalized Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,11 +7,10 @@ export default function Header() {
         <header className="header">
             <h1><a href="/"> David Cho </a></h1>
             <ul className="header-links">
-                <li><a href="#about"> About </a></li>
-                <li><a href="#skills"> Skills </a></li>
-                <li><a href="#projects"> Projects </a></li>
-                <li><a href="#resume"> Resume </a></li>
-                <li><a href="#contact"> Contact </a></li>
+                <li><a href="#about" data-replace="About"><span> About </span></a></li>
+                <li><a href="#skills" data-replace="Skills"> <span> Skills </span> </a></li>
+                <li><a href="#projects" data-replace="Projects"><span> Projects </span> </a></li>
+                <li><a href="#contact" data-replace="Contact"><span> Contact </span> </a></li>
             </ul>
         </header>
     )
